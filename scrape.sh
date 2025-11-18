5591966ce89c1b178aeace8f33d7c3a3b2e1ceab
Josh Meyers <jmeyers3@slac.stanford.edu>
2025-11-18 12:36:15 -0800

    update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -11,5 +11,7 @@ setup lsst_distrib -t w_latest
 cd /sdf/data/rubin/u/jmeyers3/projects/aos/rubintv_guide
 python scrape_blocks.py > /sdf/home/j/jmeyers3/public_html/rubintv_guide/scrape/scrape_blocks.log 2>&1
 cp blocks.json /sdf/home/j/jmeyers3/public_html/rubintv_guide/blocks.json
+cp tblock_names.json /sdf/home/j/jmeyers3/public_html/rubintv_guide/tblock_names.json
 chmod o+g /sdf/home/j/jmeyers3/public_html/rubintv_guide/blocks.json
+chmod o+g /sdf/home/j/jmeyers3/public_html/rubintv_guide/tblock_names.json
 date >> /sdf/home/j/jmeyers3/public_html/rubintv_guide/scrape/last_updated.txt
